5d7a783851fbd1652a24cfccb38117d80a953e23
Madeline Corman <madelinecorman@gmail.com>
2019-11-03 19:40:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ ["dog", "cat", "fish", "bird"] 
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ create_an_array
+ create_an_array << "lizard"
 end
 
 def add_element_to_start_of_array(array, element)
